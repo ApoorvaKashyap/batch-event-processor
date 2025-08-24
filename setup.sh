3720a2c61c4f8b861f70d0b2332c098c923bb2a0
Apoorva Kashyap <apoorvakashyap54@gmail.com>
2025-08-24 19:46:43 +0000

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# Install Prerequisites
+sudo apt install curl containerd docker.io docker-buildx
+
+# Add user to docker group
+sudo usermod -aG docker $USER && newgrp docker
+sudo systemctl enable --now docker
+
+# Install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# Install Minikube
+curl -LO "https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64"
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && rm ./kubectl
+
+# Install Argo CLI
+# Detect OS
+ARGO_OS="darwin"
+if [[ "$(uname -s)" != "Darwin" ]]; then
+  ARGO_OS="linux"
+fi
+
+# Download the binary
+curl -sLO "https://github.com/argoproj/argo-workflows/releases/download/v3.7.1/argo-$ARGO_OS-amd64.gz"
+
+# Unzip
+gunzip "argo-$ARGO_OS-amd64.gz"
+
+# Make binary executable
+chmod +x "argo-$ARGO_OS-amd64"
+
+# Move binary to path
+sudo mv "./argo-$ARGO_OS-amd64" /usr/local/bin/argo
+
+# Remove the downloaded file
+rm "argo-$ARGO_OS-amd64.gz"
+
+
+# Check Installations
+# uv
+uv --version
+
+# Docker
+docker version
+
+# Minikube
+minikube version
+
+# kubectl
+kubectl version --client
+
+# Argo Workflow CLI
+argo version
+
